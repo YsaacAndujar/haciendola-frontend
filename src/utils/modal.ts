14b1cc613c4e06c2f7298c3b5fc41925140fd013
Ysaac Andujar Binet <ysaac@codika.net>
2024-05-15 21:22:31 -0400

    return swal on showmodal

--- src/utils/modal.ts
+++ src/utils/modal.ts
@@ -1,7 +1,7 @@
 import Swal from 'sweetalert2'
 
 export const showModal= ({title, text, type}:{title:string, text:string, type: 'success' | 'warning' | 'error'}) =>{
-    Swal.fire({
+    return Swal.fire({
         title,
         text,
         icon: type
